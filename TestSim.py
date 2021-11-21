6f8538e84eed3d1f62b3f0c8ca1f2514590b143f
Shrithik Sareddy <shrithik1@gmail.com>
2021-11-20 21:04:13 -0800

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -143,12 +143,16 @@ def main():
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
-
-    s.runTime(20);
-    s.ping(1, 2, "Hello, World");
-    s.runTime(10);
-    s.ping(1, 3, "Hi!");
-    s.runTime(20);
+    s.addChannel(s.ROUTING_CHANNEL);
+    s.addChannel(s.TRANSPORT_CHANNEL);
+    s.runTime(250);
+    s.testServer(1);
+    # s.ping(1, 2, "Hello, World");
+    s.runTime(60);
+    # s.ping(1, 3, "Hi!");
+    s.testClient(4);
+    s.runTime(1);
+    s.runTime(400);
 
 if __name__ == '__main__':
     main()
