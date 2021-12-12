bba07160710b18e836f34d4dba3d17ab5c22de43
Dale Allen Alabastro <90293022+dalealabastro@users.noreply.github.com>
2021-12-12 12:28:55 -0800

    Update socket.h

--- includes/socket.h
+++ includes/socket.h
@@ -1,39 +1,55 @@
 #ifndef __SOCKET_H__
 #define __SOCKET_H__
 
-#include "protocol.h"
-#include "channels.h"
+enum{
+    MAX_NUM_OF_SOCKETS = 10,
+    ROOT_SOCKET_ADDR = 255,
+    ROOT_SOCKET_PORT = 255,
+    SOCKET_BUFFER_SIZE = 128,
+};
 
-#define BUFFER_SIZE 64
+enum socket_state{
+    CLOSED,
+    LISTEN,
+    ESTABLISHED,
+    SYN_SENT,
+    SYN_RCVD,
+};
 
-#define CLOSED      0
-#define SYN_SENT    1
-#define SYN_RCVD    2
-#define ESTABLISHED 3
-#define FIN_WAIT1   4
-#define FIN_WAIT2   5
-#define TIME_WAIT   6
-#define CLOSE_WAIT  7
-#define LAST_ACK    8
-#define LISTEN      9
 
+typedef nx_uint8_t nx_socket_port_t;
+typedef uint8_t socket_port_t;
+
+// socket_addr_t is a simplified version of an IP connection.
 typedef nx_struct socket_addr_t{
-    nx_uint16_t location;
-    nx_uint8_t port;
-} socket_addr_t;
+    nx_socket_port_t port;
+    nx_uint16_t addr;
+}socket_addr_t;
 
-typedef nx_struct socket_t{
+
+// File descripter id. Each id is associated with a socket_store_t
+typedef uint8_t socket_t;
+
+// State of a socket. 
+typedef struct socket_store_t{
+    uint8_t flag;
+    enum socket_state state;
+    socket_port_t src;
     socket_addr_t dest;
-    socket_addr_t src;
-    nx_uint16_t transfer;
-    nx_uint16_t seq;
-    nx_uint8_t CONN;
-    nx_uint8_t advertisedWindow;
-    nx_uint16_t nextExp;
-    nx_uint16_t lastRCVD;
-    nx_uint16_t lastACKED;
-    nx_uint8_t sendBuffer[BUFFER_SIZE];
-    nx_uint16_t rcvdBuffer[BUFFER_SIZE];
-    nx_uint16_t sendBuffCounter;
-} socket_t;
+
+    // This is the sender portion.
+    uint8_t sendBuff[SOCKET_BUFFER_SIZE];
+    uint8_t lastWritten;
+    uint8_t lastAck;
+    uint8_t lastSent;
+
+    // This is the receiver portion
+    uint8_t rcvdBuff[SOCKET_BUFFER_SIZE];
+    uint8_t lastRead;
+    uint8_t lastRcvd;
+    uint8_t nextExpected;
+
+    uint16_t RTT;
+    uint8_t effectiveWindow;
+}socket_store_t;
 #endif
